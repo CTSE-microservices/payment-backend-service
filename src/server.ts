c92632d733a8dc78f99e0464f4b1cec18a4fbfd7
Dulaksha-Siriwardana <dulakshasiriwardana@gmail.com>
2026-03-19 22:17:13 +0530

    chore: update prisma dependencies and configuration

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import app from "./app.js";
 import { logger } from "./utils/logger.js";
 import { connectRabbitMQ } from "./config/rabbitmqConnection.js";
+import { startOrderCreatedConsumer } from "./consumers/orderCreated.consumer.js";
 
 const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
   try {
-  
     await connectRabbitMQ();
+    await startOrderCreatedConsumer();
 
-   
     app.listen(PORT, () => {
       logger.info(`🚀 Payment Service running on port ${PORT}`);
     });
